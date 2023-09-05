18f8a71eb950f1b6d2b2d21d8f6242ab374f7613
José Vitor dos Santos Fernandes <jose-vitor-7@hotmail.com>
2023-09-05 00:32:51 -0300

    Finish auth, with github & google login

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,9 +6,12 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { signIn } from 'next-auth/react'
 
-import Input from '@/components/Input'
+import Input from '~/components/Input'
 
 import logo from '/public/images/logo.png'
+
+import { FcGoogle } from 'react-icons/fc'
+import { FaGithub } from 'react-icons/fa'
 
 const Auth = () => {
   const router = useRouter()
@@ -121,6 +124,20 @@ const Auth = () => {
             >
               {variant === 'login' ? 'Login' : 'Sign up'}
             </button>
+            <div className="flex flex-row center gap-4 mt-8 justify-center">
+              <div
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn('google', { callbackUrl: '/' })}
+              >
+                <FcGoogle size={30} />
+              </div>
+              <div
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition"
+                onClick={() => signIn('github', { callbackUrl: '/' })}
+              >
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12">
               {variant === 'login'
                 ? 'First time using Netflix?'
